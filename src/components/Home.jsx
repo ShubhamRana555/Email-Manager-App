533461993d0394eeae5953f6f7edf01c0fa537fc
Shubham <shubhamrana0108@gmail.com>
2024-10-24 07:25:10 +0530

    edit-2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -39,7 +39,7 @@ const Home = () => {
                                     Edit
                                     </Link>
                                     <button className="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-6 py-2 mx-5 my-2 dark:bg-red-600 dark:hover:bg-red-700 focus:ring-red-700 dark:focus:ring-red-700" 
-                                    onClick={()=> dispatch(deleteUser(user.id))}>
+                                    onClick={()=> dispatch(deleteUser({id: user.id}))}>
                                         Delete
                                     </button>
                                 </td>
